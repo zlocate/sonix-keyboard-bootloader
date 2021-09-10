1bb766f8426679fcb7c0c04473605a93351334eb
Adam Honse <calcprogrammer1@gmail.com>
2021-09-09 23:19:43 -0500

    Add GitHub CI

---
+++ build.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+apt update
+apt install -y cmake gcc-arm-none-eabi git
+
+git clean -dfx
+git reset --hard HEAD
+
+mkdir build
+cd build
+
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../toolchain/gcc.cmake
+
+make -j8
