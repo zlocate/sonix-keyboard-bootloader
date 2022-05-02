d9cb6f93cb2bb0b6458bb900f214651a0e39661f
fortaa <fortaa@users.noreply.github.com>
2022-05-02 10:43:11 +0000

    Add Keychron K7 White (SN32F260) (#15)
    That is an almost identical copy of the K6 White model.
    
    Co-authored-by: Aleksander Trofimowicz <trof@n90.eu>

--- src/config.h
+++ src/config.h
@@ -196,6 +196,17 @@
 #define BLKEY_OUTPUT_GPIO SN_GPIO3
 #define BLKEY_OUTPUT_PIN 11
 #define PIN_HIGH_WHEN_KEY_HELD 1
+#elif defined(TARGET_KEYCHRON_K7_WHITE_OPTICAL) // SN32F260
+ #include "SN32F260.h"
+#define SN32_BOOTLOADER_ADDRESS 0x1FFF0009
+#define BOOT0_GPIO SN_GPIO3
+#define BOOT0_PIN 5
+/* Backspace */
+#define BLKEY_INPUT_GPIO SN_GPIO0
+#define BLKEY_INPUT_PIN 13
+#define BLKEY_OUTPUT_GPIO SN_GPIO2
+#define BLKEY_OUTPUT_PIN 4
+#define PIN_HIGH_WHEN_KEY_HELD 1
 #else
 #error Not configured for this target.
 #endif
